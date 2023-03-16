64faada3937d91edc39c1a03ca45b0829506858b
Christian <christian@brendlinonline.de>
2023-03-16 22:56:15 +0100

    removed unneeded dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use clap::Parser;
-use miette::{IntoDiagnostic, Result};
 use netres::{restart_pc, Cli, Commands};
 
 fn main() {
-    tracing_subscriber::fmt::init();
-
     let cli = Cli::parse(); //TODO: Create CLI
 
     let result = match &cli.command {
